cfc25a5620850eb428923ec979989ed2a93973fa
Vincent DE MENEZES <vde-mene@e2r9p20.42.fr>
2016-01-18 18:45:33 +0100

    gros push bien norme

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -1,7 +1,18 @@
-#inlcude <stdlib.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-mene <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/18 18:41:02 by vde-mene          #+#    #+#             */
+/*   Updated: 2016/01/18 18:41:51 by vde-mene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	*ft_memalloc(size_t size)
+#include "libft.h"
+
+void		*ft_memalloc(size_t size)
 {
 	char	*str;
 
@@ -12,18 +23,4 @@ void	*ft_memalloc(size_t size)
 	}
 	memset(mem, 0, size);
 	return (mem);
-}
-
-int	main(void)
-{
-	char	*str;
-	int	i;
-
-	i = 0;
-	*str = ft_memalloc(10);
-	if (*str != '\0')
-	{
-		printf("sa passe");
-	}	
-	return (0);
 }
